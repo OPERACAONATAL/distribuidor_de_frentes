b94e720b60b4659847b74d905245e7a1ef094048
Fazendaaa <lucas.carotta@outlook.com>
2017-09-13 22:55:56 -0300

    Corrigido opções dos horários.

--- distribuidor.py
+++ distribuidor.py
@@ -6,17 +6,17 @@ import math
 # Padrão para remover vírgulas e espaços
 pattern = re.compile("^\s+|\s*,\s*|\s+$")    
 teams = ['Endomarketing', 'Fotografia', 'Programação', 'Vídeo', 'Workshop']
-dates_times = ['Seg 12h30min -- 14h', 'Seg 18h - 19h', 'Seg 19 - 20h', 'Seg 20 - 21h', 'Seg 21 - 22h', 'Ter 12h30min - 14h', 'Ter 18h - 19h', 'Ter 19 - 20h', 'Ter 20 - 21h', 'Ter 21 - 22h', 'Qua 12h30min - 14h', 'Qua 18h - 19h', 'Qua 19 - 20h', 'Qua 20 - 21h', 'Qua 21 - 22h', 'Qui 12h30min - 14h', 'Qui 20 - 21h', 'Qui 21 - 22h', 'Sábado ou Domingo']
+dates_times = ['Seg 12h30min -- 14h', 'Seg 18h - 19h', 'Seg 19h - 20h', 'Seg 20h - 21h', 'Seg 21h - 22h', 'Ter 12h30min - 14h', 'Ter 18h - 19h', 'Ter 19h - 20h', 'Ter 20h - 21h' 'Ter 21h - 22h', 'Qua 12h30min - 14h', 'Qua 19h - 20h', 'Qua 20h - 21h', 'Qua 21h - 22h', 'Qui 12h30min - 14h', 'Qui 19h - 20h', 'Qui 20h - 21h', 'Qui 21h - 22h', 'Sábado ou Domingo']
 
 def strip(arg, array):
     values = copy.deepcopy(array)
-    options = sorted([x for x in pattern.split(arg) if x])
+    options = [x for x in pattern.split(arg) if x]
 
     for i in range(0, len(values)):
-        if values[i] not in options:
-            values[i] = math.nan
-        else:
+        if values[i] in options:
             values[i] = 1
+        else:
+            values[i] = math.nan
 
     return values
 
@@ -27,6 +27,7 @@ def strip_date_time(arg):
     return strip(arg, dates_times)
 
 df = pd.read_csv('answers.csv')
+# Remove a coluna do horário da resposta
 df.drop(df.columns[0], axis=1, inplace=True)
 
 df['Frentes'] = df.pop('Qual frente você gostaria de participar nesse primeiro momento?').apply(strip_teams)
